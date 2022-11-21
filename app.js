cd75a577f3d513a822722d67d43d6057929a00e9
rzarzayev <rrzayev792@gmail.com>
2022-11-21 11:55:10 +0400

    add

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const box1 = document.querySelector(".box1");
 const box2 = document.querySelector(".box2");
 const curInput1 = document.querySelector(".cur-input-1");
 const curInput2 = document.querySelector(".cur-input-2");
-const btn = document.querySelectorAll("button");
+const leftButtons = document.querySelectorAll(".box1 button");
+const rightButtons = document.querySelectorAll(".box2 button");
+
 
 const rateBox = document.querySelector(".rate-box");
 const changeBtn = document.querySelector(".fa-retweet");
@@ -29,7 +31,7 @@ function listeners() {
   box2.addEventListener("change", calc);
   curInput1.addEventListener("input", calc);
   curInput2.addEventListener("input", calc);
-  btn.addEventListener("click", calc);
+
   changeBtn.addEventListener("click", () => {
     [box1.value, box2.value] = [box1.value, box2.value];
     calc();
@@ -42,9 +44,32 @@ function listeners() {
 //   calc();
 // };
 
-function addBtn() {
-  const arr = ["RUB", "USD", "EUR", "GBP"];
-  arr.forEach((soz)=>{
-    const btn = document.createElement()
-  })
+const url = 'https://api.exchangerate.host/latest?'
+let leftSideBaseCurrency = ''
+
+function onChangeCurrency(event,whichSide,currency){
+    
+    if(whichSide === 'left'){
+        leftSideBaseCurrency = currency;
+    [...leftButtons].forEach((button) => {
+        button.classList.remove('activeButton')
+        
+    })
+    }
+    if(whichSide === 'right'){
+        fetch(`${url}base=${leftSideBaseCurrency}&symbols=${currency}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            const v1 = curInput1.value
+            const mezenne = data.
+            curInput2.value = v1*mezenne
+        });
+        [...leftButtons].forEach((button) => {
+            button.classList.remove('activeButton')
+            
+        })
+        }
+        event.target.classList.add('activeButton')
+    console.log(whichSide,currency)
 }
